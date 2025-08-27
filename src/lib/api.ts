b4cb3bc873bdb9e45645090660543d2d8cc310d0
Talgat Medetbekov <medetbekov.t@gmail.com>
2025-08-27 15:27:38 +0600

    Use axios

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,36 +1,36 @@
-const MIDGARD = process.env.NEXT_PUBLIC_MIDGARD_API
-const THORNODE = process.env.NEXT_PUBLIC_THOR_API
+import axios from 'axios'
+
+const midgard = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_MIDGARD_API
+})
+
+const thornode = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_THOR_API
+})
 
 export const getPools = async () => {
-  const res = await fetch(`${MIDGARD}/v2/pools`)
+  return midgard
+    .get('/v2/pools')
+    .then(res => res.data)
+    .then((data: any) =>
+      data.map((item: any) => {
+        const [chain, asset] = item.asset.split('.')
+        const [symbol] = asset.split('-')
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch transactions')
-  }
-
-  return (await res.json()).map((item: any) => {
-    const [chain, asset] = item.asset.split('.')
-    const [symbol] = asset.split('-')
-
-    return {
-      type: chain === 'THOR' ? 'NATIVE' : 'LAYER_1',
-      asset: item.asset,
-      chain,
-      metadata: {
-        symbol,
-        decimals: item.nativeDecimal
-      }
-    }
-  })
+        return {
+          type: chain === 'THOR' ? 'NATIVE' : 'LAYER_1',
+          asset: item.asset,
+          chain,
+          metadata: {
+            symbol,
+            decimals: item.nativeDecimal
+          }
+        }
+      })
+    )
 }
 
 export const getQuote = async (params: Record<string, any>) => {
   const qs = new URLSearchParams(Object.entries(params).filter(i => i[1]))
-  const res = await fetch(`${THORNODE}/thorchain/quote/swap?${qs.toString()}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch transactions')
-  }
-
-  return await res.json()
+  return thornode.get(`/thorchain/quote/swap?${qs.toString()}`).then(res => res.data)
 }
