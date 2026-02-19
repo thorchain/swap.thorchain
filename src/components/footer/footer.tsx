ae1a90233e0746912a6113dc716b1d07e2ae8895
Talgat Medetbekov <medetbekov.t@gmail.com>
2026-02-19 12:09:36 +0600

    Update Footer design

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,50 +6,27 @@ export function Footer() {
   return (
     <footer className="bg-tyler fixed inset-x-0 bottom-0 mx-auto hidden md:block">
       <div className="container mx-auto border-t p-4">
-        <div className="flex items-center justify-between gap-4">
-          <div className="text-thor-gray flex items-center gap-1 text-xs">
-            <span>Built by</span>
-            <Icon name="unstoppable" className="size-3" />
-            <a className="underline" href="https://x.com/unstoppablebyhs" rel="noopener noreferrer" target="_blank">
-              Unstoppable Wallet
+        <div className="text-thor-gray flex items-center justify-between gap-4 text-xs">
+          <div className="flex h-4 items-center gap-2">
+            <a href={AppConfig.privacyPolicyLink} rel="noopener noreferrer" target="_blank">
+              Privacy Policy
             </a>
-          </div>
-
-          <div className="text-thor-gray flex items-center gap-3 text-xs">
-            <div className="flex h-4 items-center gap-2">
-              <a href={AppConfig.privacyPolicyLink} rel="noopener noreferrer" target="_blank">
-                Privacy Policy
-              </a>
-              <Separator orientation="vertical" className="bg-andy h-full" />
-              <a href={AppConfig.tosLink} rel="noopener noreferrer" target="_blank">
-                Terms of Use
-              </a>
-              <Separator orientation="vertical" className="bg-andy h-full" />
-              <span>
-                Report bugs to{' '}
-                <a className="underline" href={`mailto:${AppConfig.supportEmail}`}>
-                  {AppConfig.supportEmail}
-                </a>
-              </span>
-              <a href={`mailto:${AppConfig.supportEmail}`}>
-                <Icon width={20} height={20} viewBox="0 0 20 20" name="email" />
+            <Separator orientation="vertical" className="bg-andy h-full" />
+            <a href={AppConfig.tosLink} rel="noopener noreferrer" target="_blank">
+              Terms of Use
+            </a>
+            <Separator orientation="vertical" className="bg-andy h-full" />
+            <div className="flex items-center gap-1">
+              <span>Built by</span>
+              <Icon name="unstoppable" className="size-3" />
+              <a className="underline" href="https://x.com/unstoppablebyhs" rel="noopener noreferrer" target="_blank">
+                Unstoppable Wallet
               </a>
             </div>
-            {(AppConfig.discordLink || AppConfig.telegramLink) && (
-              <div className="flex items-center gap-2">
-                {AppConfig.discordLink && (
-                  <a href={AppConfig.discordLink} rel="noopener noreferrer" target="_blank">
-                    <Icon width={20} height={20} viewBox="0 0 20 20" name="discord" />
-                  </a>
-                )}
-                {AppConfig.telegramLink && (
-                  <a href={AppConfig.telegramLink} rel="noopener noreferrer" target="_blank">
-                    <Icon width={20} height={20} viewBox="0 0 20 20" name="telegram" />
-                  </a>
-                )}
-              </div>
-            )}
           </div>
+          <a className="flex items-center gap-2 underline" href={AppConfig.discordLink} rel="noopener noreferrer" target="_blank">
+            Get Support in Discord <Icon width={20} height={20} viewBox="0 0 20 20" name="discord" />
+          </a>
         </div>
       </div>
     </footer>
