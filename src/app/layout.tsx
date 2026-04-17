bec6a82146970bd5605af61b713f9839dd3d9ad3
Talgat Medetbekov <medetbekov.t@gmail.com>
2026-04-17 18:58:26 +0600

    Add gtag and Hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Manrope } from 'next/font/google'
 import Script from 'next/script'
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 import { ThemeProvider } from 'next-themes'
 import { Toaster } from '@/components/ui/sonner'
 import { ReactQueryProvider } from '@/components/react-query/react-query-provider'
@@ -60,6 +60,14 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en" data-brand={AppConfig.id} suppressHydrationWarning>
       {AppConfig.gtag && <GoogleTagManager gtmId={AppConfig.gtag} />}
+      <GoogleAnalytics gaId="G-VZ0FQ1WC7G" />
+      <Script
+        id="hotjar"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:6592863,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+        }}
+      />
       {AppConfig.pixelId && (
         <Script
           dangerouslySetInnerHTML={{
