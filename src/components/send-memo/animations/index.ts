106f9fa7614d6a920f6ec1768052fcbf56b6775b
Talgat Medetbekov <medetbekov.t@gmail.com>
2026-04-20 14:53:46 +0600

    Design memo-menu and add animation

---
+++ src/components/send-memo/animations/index.ts
@@ -0,0 +1,4 @@
+export const swapAnim = require('./01_SWAP.json')
+export const memoAnim = require('./02_MEMO.json')
+export const bondAnim = require('./03_BOND.json')
+export const tcyAnim = require('./04_$TCY.json')
