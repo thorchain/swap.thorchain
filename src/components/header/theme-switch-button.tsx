ea95fc9120a93ee4f9324e20e8c475d618ceca4d
Talgat Medetbekov <medetbekov.t@gmail.com>
2026-05-12 15:48:22 +0600

    Render theme-switch-button into SSR

--- src/components/header/theme-switch-button.tsx
+++ src/components/header/theme-switch-button.tsx
@@ -1,30 +1,22 @@
 'use client'
 
-import { useEffect, useState } from 'react'
 import { useTheme } from 'next-themes'
 import { Icon } from '@/components/icons'
 import { ThemeButton } from '@/components/theme-button'
 
 export const ThemeSwitchButton = () => {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return null
-  }
+  const { resolvedTheme, setTheme } = useTheme()
+  const isDark = resolvedTheme === 'dark'
 
   return (
     <ThemeButton
       variant="circleSmallOutline"
       onClick={() => {
-        setTheme(theme === 'light' ? 'dark' : 'light')
+        setTheme(isDark ? 'light' : 'dark')
       }}
+      suppressHydrationWarning
     >
-      {theme === 'light' ? <Icon name="light-mode" /> : <Icon name="dark-mode" />}
+      <span suppressHydrationWarning>{isDark ? <Icon name="dark-mode" /> : <Icon name="light-mode" />}</span>
     </ThemeButton>
   )
 }
