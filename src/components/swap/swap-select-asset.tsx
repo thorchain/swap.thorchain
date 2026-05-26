31c15c75d24fb8566967775b06e5f760f87ca5b0
Talgat Medetbekov <medetbekov.t@gmail.com>
2026-05-26 15:45:59 +0600

    Move back the "secured" badge

--- src/components/swap/swap-select-asset.tsx
+++ src/components/swap/swap-select-asset.tsx
@@ -291,6 +291,12 @@ export const SwapSelectAsset = ({ isOpen, onOpenChange, selected, onSelectAsset 
                             <div className="text-left">
                               <div className="text-txt-high-contrast flex max-w-40 items-center gap-1.5 truncate font-semibold">
                                 <span>{asset.ticker}</span>
+                                {asset.isSecuredAsset && (
+                                  <span className="border-gray text-txt-label-small rounded-full border px-1.5 text-[10px] font-medium">
+                                    {t('selectAsset.secured')}
+                                  </span>
+                                )}
+
                                 {asset.isTradeAsset && (
                                   <span className="border-gray text-txt-label-small rounded-full border px-1.5 text-[10px] font-medium">
                                     {t('selectAsset.trade')}
@@ -299,11 +305,6 @@ export const SwapSelectAsset = ({ isOpen, onOpenChange, selected, onSelectAsset 
                               </div>
                               <div className="text-txt-label-small flex items-center gap-1.5 text-sm">
                                 <span>{chainLabel(asset.chain)}</span>
-                                {asset.isSecuredAsset && (
-                                  <span className="border-gray rounded-full border px-1.5 text-[10px] font-medium">
-                                    {t('selectAsset.secured')}
-                                  </span>
-                                )}
                               </div>
                             </div>
                           </div>
