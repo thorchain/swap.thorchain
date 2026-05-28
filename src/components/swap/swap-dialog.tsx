b6f7ebfe1b039e95ee67da29c6ed1027b4521e52
Talgat Medetbekov <medetbekov.t@gmail.com>
2026-05-28 13:03:29 +0600

    Make confirm screen little bigger

--- src/components/swap/swap-dialog.tsx
+++ src/components/swap/swap-dialog.tsx
@@ -109,7 +109,7 @@ export const SwapDialog = ({ provider, isOpen, onOpenChange }: SwapDialogProps) 
 
   return (
     <Credenza open={isOpen} onOpenChange={onOpenChange}>
-      <CredenzaContent className="flex h-auto max-h-5/6 flex-col md:max-w-xl">
+      <CredenzaContent className="flex h-auto max-h-5/6 flex-col md:max-w-2xl">
         {quote ? (
           <>
             <SwapConfirm quote={quote} priceImpact={priceImpact} />
